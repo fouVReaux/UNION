460450f4a934e93293a970867cfcfd5eca3330a2
Lavrat Cyril <10447082+fouVReaux@users.noreply.github.com>
2020-01-25 00:01:03 +0100

    Correction of the tutotial

--- tutorial/8-DistributedDrumMachine/src/clients/player/PlayerExperience.js
+++ tutorial/8-DistributedDrumMachine/src/clients/player/PlayerExperience.js
@@ -32,7 +32,12 @@ class PlayerExperience extends Experience {
 
     this.listeners = {
       toggleStep: (scoreName, index) => {
-        // ... update score when a checkbox is clicked
+        //console.log(scoreName, index);
+        const scores = this.globals.get('scores');
+        const score  = scores[scoreName];
+        score[index]= 1 - score[index];
+
+        this.globals.set({'scores': scores});
       },
     };
 
@@ -43,10 +48,41 @@ class PlayerExperience extends Experience {
     });
 
     // init engines according to score
+    const bpm = this.globals.get('bpm');
+    const scores = this.globals.get('scores');
+    const buffers = this.audioBufferLoader.data;
+    const period = 60 / bpm;
+
+    const syncTime = this.sync.getSyncTime();
+    const nextStepTime = Math.floor(syncTime/period)*period + period;
+
+    for (let name in scores){
+      const score = scores[name];
+      const buffer = buffers[name];
+      const activeStep = this.checkin.state.get('index')%score.length;
+      const engine = new StepEngine(this.audioContext, period, buffer, activeStep);
+      engine.connect(this.audioContext.destination);
+      engine.score = score;
+
+      scheduler.add(engine, nextStepTime);
+
+      this.engines[name]=engine;
+    };
 
     // wait for state updates
     this.globals.subscribe(updates => {
-      // ....
+      for(let name in updates){
+        switch (name) {
+          case 'scores':
+          const scores = updates['scores']
+            for(let instr in scores){
+              this.engines[instr].score=scores[instr]
+            }
+            break;
+          default:
+
+        }
+      }
 
       this.renderApp();
     });
@@ -63,6 +99,26 @@ class PlayerExperience extends Experience {
         <h1 class="title">index: ${clientIndex}</h1>
 
         <!-- display score and bind events -->
+        ${Object.keys(scores).map(name=>{
+          const score = scores[name];
+
+          return html`
+            <div>
+              <label style="display:inline-block; width: 50px;">
+                ${name}
+              </label>
+              ${score.map((value, index)=> {
+                return html`
+                  <input
+                    type="checkbox"
+                    .checked="${value === 1}"
+                    @click="${e => this.listeners.toggleStep(name, index)}"
+                  />
+                `;
+              })}
+            </div>
+          `;
+        })}
       </div>
     `, this.$container);
   }
--- tutorial/8-DistributedDrumMachine/src/clients/player/StepEngine.js
+++ tutorial/8-DistributedDrumMachine/src/clients/player/StepEngine.js
@@ -16,8 +16,17 @@ class StepEngine {
 
   advanceTime(syncTime) {
     const audioTime = this.master.audioTime;
+    const step = Math.round(syncTime / this.period) % this.score.length;
 
     // play buffer at right step
+    //if (step === this.activeStep && this.score[step] === 1){
+    if (this.score[step] === 1){
+
+      const src = this.audioContext.createBufferSource();
+      src.connect(this.output);
+      src.buffer = this.buffer;
+      src.start(audioTime);
+    }
 
     return syncTime + this.period;
   }
